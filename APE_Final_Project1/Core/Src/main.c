2aa53d2e8dd3348bc3931d07ce8c911974c356da
conradbuck <buckconrad0@gmail.com>
2024-12-13 01:25:45 -0800

    build finished

--- APE_Final_Project1/Core/Src/main.c
+++ APE_Final_Project1/Core/Src/main.c
@@ -21,8 +21,9 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "libraries/Ape_Final_Project1/libraries/stm32-ssd1306/ssd1306/ssd1306.h"
 #include "ssd1306_tests.h"
+#include "ssd1306.h"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -42,9 +43,13 @@
 
 /* Private variables ---------------------------------------------------------*/
 
+
 SPI_HandleTypeDef hspi1;
-I2C_HandleTypeDef hi2c1;
+//
 UART_HandleTypeDef huart1;
+GPIO_TypeDef *I2C_Led_GPIO_Port = GPIOA;
+GPIO_TypeDef *I2C_Led_Pin = GPIO_PIN_4;
+I2C_HandleTypeDef hi2c1;
 
 /* USER CODE BEGIN PV */
 
@@ -53,7 +58,7 @@ UART_HandleTypeDef huart1;
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_I2C1_SMBUS_Init(void);
+static void MX_I2C1_Init(void);
 static void MX_SPI1_Init(void);
 static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
@@ -68,9 +73,11 @@ static void MX_USART1_UART_Init(void);
 /**
   * @brief  The application entry point.
   * @retval int
+  *
   */
 int main(void)
 {
+	//GPIOA;
 
   /* USER CODE BEGIN 1 */
 
@@ -82,6 +89,7 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
+
   //HAL_GPIO_WritePin(SPI_Led_GPIO_Port, SPI_Led_Pin, GPIO_PIN_RESET);
 	HAL_GPIO_WritePin(I2C_Led_GPIO_Port, I2C_Led_Pin, GPIO_PIN_RESET);
   /* USER CODE END Init */
@@ -90,19 +98,17 @@ int main(void)
   SystemClock_Config();
 
   /* USER CODE BEGIN SysInit */
-  static void MX_GPIO_Init(void);
-  static void MX_SPI1_Init(void);
-  static void MX_I2C1_Init(void);
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_I2C1_SMBUS_Init();
+  MX_I2C1_Init();
   MX_SPI1_Init();
   MX_USART1_UART_Init();
-  MX_SPI1_Init();
-  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
+
+  //OLED TEST Function
+  ssd1306_TestAll();
 
   /* USER CODE END 2 */
 
@@ -111,29 +117,10 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-    String GPSLoc = ReadGPS();  
-    OutPutToLCDScreen(GPSLoc);
-    
-    HAL_Delay(150);
 
-    String IMULoc = ReadIMU();
-    OutPutToLCDScreen(IMULoc);
-    wait();    
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
-}
-
-void OutPutToLCDScreen(String){
-
-}
-
-int ReadIMU(){
-
-}
-
-int ReadGPS(){
-
 }
 
 /**
@@ -179,7 +166,7 @@ void SystemClock_Config(void)
   * @param None
   * @retval None
   */
-static void MX_I2C1_SMBUS_Init(void)
+static void MX_I2C1_Init(void)
 {
 
   /* USER CODE BEGIN I2C1_Init 0 */
@@ -189,6 +176,19 @@ static void MX_I2C1_SMBUS_Init(void)
   /* USER CODE BEGIN I2C1_Init 1 */
 
   /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
   /* USER CODE BEGIN I2C1_Init 2 */
 
   /* USER CODE END I2C1_Init 2 */
